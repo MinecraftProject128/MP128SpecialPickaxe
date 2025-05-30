b87db9b6a4f3dfb57fc89d1a8e4aa8db860c95eb
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-05-30 22:33:29 +0300

    Начальный коммит

---
+++ src/main/java/project128/minecraft/mP128SpecialPickaxe/MP128SpecialPickaxe.java
@@ -0,0 +1,17 @@
+package project128.minecraft.mP128SpecialPickaxe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MP128SpecialPickaxe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
