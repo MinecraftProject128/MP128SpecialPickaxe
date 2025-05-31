0e906d10915991916572eb76f7ccd5dbc9c12c26
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-05-31 19:49:28 +0300

    Выдаётся кирка с описанием

--- src/main/java/project128/minecraft/mP128SpecialPickaxe/commands/GiveSpecialPickaxe.java
+++ src/main/java/project128/minecraft/mP128SpecialPickaxe/commands/GiveSpecialPickaxe.java
@@ -1,11 +1,16 @@
 package project128.minecraft.mP128SpecialPickaxe.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import project128.minecraft.mP128SpecialPickaxe.config.SpecialPickaxe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GiveSpecialPickaxe {
     private static GiveSpecialPickaxe instance;
@@ -22,14 +27,6 @@ public class GiveSpecialPickaxe {
         return instance;
     }
 
-    private ItemMeta setupPickaxeMeta(ItemMeta meta, SpecialPickaxe data) {
-        meta.displayName(LegacyComponentSerializer.legacySection().deserialize(
-                "§r" + data.getDisplayName().replace("&", "§")
-        ));
-
-        return meta;
-    }
-
     public void givePickaxe(Player player, SpecialPickaxe data) {
         ItemStack pickaxe = new ItemStack(SpecialPickaxe.PickaxeMaterial.toPickaxe(
                 data.getMaterial()
@@ -38,5 +35,26 @@ public class GiveSpecialPickaxe {
 
         player.getInventory().addItem(pickaxe);
     }
+
+    // TODO: Разобраться как убирать курсив по желанию пользователя
+    private ItemMeta setupPickaxeMeta(ItemMeta meta, SpecialPickaxe data) {
+        meta.displayName(LegacyComponentSerializer.legacySection().deserialize(
+                "§r" + data.getDisplayName().replace("&", "§")
+        ).decoration(TextDecoration.ITALIC, false));
+        meta.lore(formatLegacyListToComponent(data.getDescription()));
+        return meta;
+    }
+
+    // TODO: Разобраться как убирать курсив по желанию пользователя
+    private List<Component> formatLegacyListToComponent(List<String> legacy) {
+        List<Component> newList = new ArrayList<>();
+        for (String line : legacy) {
+            newList.add(LegacyComponentSerializer.legacySection().deserialize(
+                    "§r" + line.replace("&", "§")
+            ).decoration(TextDecoration.ITALIC, false));
+        }
+        return newList;
+    }
+
 
 }
