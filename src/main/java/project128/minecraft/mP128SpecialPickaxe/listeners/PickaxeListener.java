c96781cc256072bd87f0ddcd77afab427e08d53a
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-02 18:38:48 +0300

    Отрисовка линии

--- src/main/java/project128/minecraft/mP128SpecialPickaxe/listeners/PickaxeListener.java
+++ src/main/java/project128/minecraft/mP128SpecialPickaxe/listeners/PickaxeListener.java
@@ -1,6 +1,10 @@
 package project128.minecraft.mP128SpecialPickaxe.listeners;
 
+import it.unimi.dsi.fastutil.Pair;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +12,11 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
+import project128.minecraft.mP128SpecialPickaxe.config.Config;
+import project128.minecraft.mP128SpecialPickaxe.config.SpecialPickaxe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PickaxeListener implements Listener {
     private final Plugin plugin;
@@ -21,9 +30,43 @@ public class PickaxeListener implements Listener {
         Player player = event.getPlayer();
         ItemStack usedItem = player.getInventory().getItemInMainHand();
         NamespacedKey namespacedKey = NamespacedKey.fromString("specialpickaxe", plugin);
-        if (namespacedKey != null)
-            plugin.getLogger().info(usedItem.getItemMeta().getPersistentDataContainer()
-                    .get(namespacedKey, PersistentDataType.STRING));
+        if (namespacedKey != null) {
+            String name = usedItem.getItemMeta().getPersistentDataContainer()
+                    .get(namespacedKey, PersistentDataType.STRING);
+            if (name == null)
+                return;
+            SpecialPickaxe data = Config.getInstance(plugin)
+                    .getPickaxe(name);
+            removeBlocks(event.getBlock().getLocation(), data);
+        }
+    }
+
+    private List<Pair<Integer, Integer>> getLine(double angle, int length, boolean mirror) {
+        List<Pair<Integer, Integer>> points = new ArrayList<>();
+        int l = 0;
+        if (mirror)
+            l = -length;
+        while (l <= length) {
+            points.add(Pair.of(
+                    ((Long) (Math.round(Math.cos(angle)) * l)).intValue(),
+                    ((Long) (Math.round(Math.sin(angle)) * l)).intValue()
+            ));
+            l += 1;
+        }
+        return points;
+    }
+
+    private void removeBlocks(Location started, SpecialPickaxe data) {
+        World w = started.getWorld();
+        Integer sx = started.getBlockX();
+        Integer sy = started.getBlockY();
+        Integer sz = started.getBlockZ();
+        List<Pair<Integer, Integer>> line = getLine(0, data.getShape().length(), false);
+        List<Pair<Integer, Integer>> perpendicular = getLine(Math.PI / 2, data.getShape().width(), true);
+        for (Pair<Integer, Integer> masterPoint : line) {
+            new Location(w, sx + masterPoint.first(), sy, sz + masterPoint.second())
+                    .getBlock().setType(Material.GLASS);
+        }
     }
 
 }
