02d3359c0c351a2976b1d52597323cbf045dc5e9
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-06-02 19:08:13 +0300

    Кирка ломает блоки

--- src/main/java/project128/minecraft/mP128SpecialPickaxe/listeners/PickaxeListener.java
+++ src/main/java/project128/minecraft/mP128SpecialPickaxe/listeners/PickaxeListener.java
@@ -61,25 +61,26 @@ public class PickaxeListener implements Listener {
     private void removeBlocks(Location started, double angle, SpecialPickaxe data) {
         World w = started.getWorld();
         int sx = started.getBlockX();
-        int sy = started.getBlockY();
         int sz = started.getBlockZ();
         List<Pair<Integer, Integer>> line = getLine(angle, data.getShape().length(), false);
         List<Pair<Integer, Integer>> perpendicular = getLine(angle + Math.PI / 2, data.getShape().width(), true);
-        new Location(w, sx, sy, sz)
-                .getBlock().setType(Material.GLASS);
-        for (Pair<Integer, Integer> masterPoint : line) {
-            new Location(w, sx - masterPoint.second(), sy, sz + masterPoint.first())
-                    .getBlock().setType(Material.GLASS);
-            for (Pair<Integer, Integer> perpPoint : perpendicular) {
-                new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
-                        sz + masterPoint.first() + perpPoint.first())
-                        .getBlock().setType(Material.GLASS);
-                new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
-                        sz + masterPoint.first() + perpPoint.first() + 1)
-                        .getBlock().setType(Material.GLASS);
-                new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
-                        sz + masterPoint.first() + perpPoint.first() - 1)
-                        .getBlock().setType(Material.GLASS);
+        new Location(w, sx, started.getBlockY(), sz)
+                .getBlock().breakNaturally();
+        for (int sy = started.getBlockY(); sy < started.getBlockY() + data.getShape().height(); sy++) {
+            for (Pair<Integer, Integer> masterPoint : line) {
+                new Location(w, sx - masterPoint.second(), sy, sz + masterPoint.first())
+                        .getBlock().breakNaturally();
+                for (Pair<Integer, Integer> perpPoint : perpendicular) {
+                    new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
+                            sz + masterPoint.first() + perpPoint.first())
+                            .getBlock().breakNaturally();
+                    new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
+                            sz + masterPoint.first() + perpPoint.first() + 1)
+                            .getBlock().breakNaturally();
+                    new Location(w, sx - masterPoint.second() - perpPoint.second(), sy,
+                            sz + masterPoint.first() + perpPoint.first() - 1)
+                            .getBlock().breakNaturally();
+                }
             }
         }
     }
